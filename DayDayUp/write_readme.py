0c02d78ca366326b3e73e1e5b043ab1052b4ed5b
Starky <david990917@qq.com>
2020-06-02 12:15:57 +0800

    python script for auto generate MD

---
+++ DayDayUp/write_readme.py
@@ -0,0 +1,19 @@
+import os
+
+ROOT=os.getcwd()
+files=os.listdir(ROOT)
+md_files=[i for i in files if i[0]=='D' and i[-3:]=='.md']
+md_files.sort(key=lambda x: int(x.split('-')[0][3:]),reverse=True)
+
+f=open("README.md",'w',encoding='utf-8')
+f.write("""
+# DayDayUp
+
+> Starky 每日奋斗文档
+""")
+
+for file in md_files:
+	filename=file[:-3]
+	f.write("### [{}]({})\n".format(filename,file))
+
+f.close()
